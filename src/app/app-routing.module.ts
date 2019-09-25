f9e17c5a606172517a3bcfeea32ddea1da59cd72
jorge salgado <jasen192021@gmail.com>
2019-09-25 11:48:40 -0500

    academic,shop,estudiantes,administration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { TemplateComponent } from './template/template.component';
 
 
 const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent
-  },
+  {    path: 'login',    component: LoginComponent  },
+
+  { path:'pages',component:TemplateComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+import { TemplateComponent } from './template/template.component';
+import { AcademicComponent } from './pages/academic/academic.component';
+import { AdministrationComponent } from './pages/administration/administration.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { EstudiantesComponent } from './pages/estudiantes/estudiantes.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent],
+    LoginComponent,
+    TemplateComponent,
+    AcademicComponent,
+    AdministrationComponent,
+    ShopComponent,
+    EstudiantesComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
